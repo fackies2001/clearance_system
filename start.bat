45bb1eb5ec8802d2d747d005a3d7ae81ef6680c3
vincent <fabayjohnvincent@gmail.com>
2026-06-09 09:55:01 +0800

    chore: remove local dev files from repo

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start "Caddy" cmd /k "cd /d D:\clearance_system && caddy.exe run"
-timeout /t 2
-start "Laravel" cmd /k "cd /d D:\clearance_system && php artisan serve --host=127.0.0.1 --port=8001"
-timeout /t 2
-start "Vite" cmd /k "cd /d D:\clearance_system && npm run dev"
